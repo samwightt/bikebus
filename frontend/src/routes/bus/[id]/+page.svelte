e5a76c2de4ba6bd5d52a3f1f41bba6ecfcfefa5a
Sam Wight <me@samw.dev>
2023-09-22 13:15:45 -0500

    Add GeoJSON displaying.

--- frontend/src/routes/bus/[id]/+page.svelte
+++ frontend/src/routes/bus/[id]/+page.svelte
@@ -7,20 +7,74 @@
 	$: ({ BusInfo } = data)
 
 	$: bus = $BusInfo.data?.bus
+	$: route = bus?.route && JSON.parse(bus?.route)
 
 	let div: HTMLElement
+	let map: maplibregl.Map | null = null
 
 	onMount(() => {
-		const map = new maplibregl.Map({
+		map = new maplibregl.Map({
 			container: div,
 			style: "https://tiles.stadiamaps.com/styles/alidade_smooth.json",
 			zoom: 15
 		})
 
 		return () => {
-			map.remove()
+			map?.remove()
 		}
 	})
+
+	$: coordinates = route && route.features[0].geometry.coordinates
+	$: bounds = coordinates ? new maplibregl.LngLatBounds({
+		lat: coordinates[0][0],
+		lng: coordinates[0][1]
+	}) : null
+	$: {
+		 if (coordinates)  {
+				for (let coordinate of coordinates) {
+					bounds?.extend(coordinate)
+				}
+		}
+	}
+
+	function recenter() {
+		if (bounds && map) {
+			map?.fitBounds(bounds, {
+				padding: 20,
+				animate: false
+			})
+		}
+	}
+
+	let loaded = false
+	$: map?.on('load', () => {
+		loaded = true
+	})
+	$: {
+		if (loaded) {
+			if (map?.getSource('route')) {
+				map?.removeSource('route');
+			}
+			map?.addSource('route', {
+				type: 'geojson',
+				data: route
+			})
+			map?.addLayer({
+				id: 'route',
+				type: 'line',
+				source: 'route',
+				'layout': {
+					'line-join': 'round',
+					'line-cap': 'round'
+				},
+				'paint': {
+					'line-color': "#000",
+					'line-width': 8
+				}
+			})
+			recenter()
+		}
+	}
 
 </script>
 
