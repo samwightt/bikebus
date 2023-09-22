7886c6a651ef97bcf62e13690dbd88163c245ec5
Sam Wight <me@samw.dev>
2023-09-22 10:21:34 -0500

    Add map to page.

--- frontend/src/routes/bus/[id]/+page.svelte
+++ frontend/src/routes/bus/[id]/+page.svelte
@@ -1,13 +1,39 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import type { PageData } from './$houdini'
+	import * as maplibregl from 'maplibre-gl'
 
 	export let data: PageData
 	$: ({ BusInfo } = data)
 
 	$: bus = $BusInfo.data?.bus
+
+	let div: HTMLElement
+
+	onMount(() => {
+		const map = new maplibregl.Map({
+			container: div,
+			style: "https://tiles.stadiamaps.com/styles/alidade_smooth.json",
+			zoom: 15
+		})
+
+		return () => {
+			map.remove()
+		}
+	})
+
 </script>
 
 <h1 class="text-5xl mb-5 font-bold">{bus?.name}</h1>
 {#if bus?.description}
 	<p class="text-xl">{bus?.description}</p>
 {/if}
+
+<div bind:this={div} />
+
+<style>
+	div {
+		max-height: 70vh;
+		height: 70vh;
+	}
+</style>
