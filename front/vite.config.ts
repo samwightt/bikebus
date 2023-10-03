ba24e9676e0aae3374080b2adf4e11147b5bed8c
Sam Wight <me@samw.dev>
2023-10-03 10:44:40 -0500

    Add tsconfig paths.

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import generouted from '@generouted/react-router/plugin'
+import tsconfig from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact(), generouted()],
+  plugins: [tsconfig(), preact(), generouted()],
 })
