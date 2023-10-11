f412c8e8fda4fa9ee1bbd1e0c2acfb269169e31b
Sam Wight <me@samw.dev>
2023-10-11 09:57:48 -0500

    Remove old frontend.

--- frontend/src/routes/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import "../app.css"
-</script>
-
-<div class="container mx-auto max-w-5xl p-8">
-<slot />
-</div>
--- frontend/src/routes/bus/+layout.svelte
+++
@@ -1,3 +0,0 @@
-<p class="text-lg text-gray-400 hover:underline mb-4"><a href="/" title="Home">BikeB.us</a></p>
-
-<slot />
